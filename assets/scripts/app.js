9760113863e4a53547abf49b39c26a8773138340
eelopez515 <eelopez15@gmail.com>
2020-09-17 21:23:40 -0400

    authentication
    Began making the form fields for the authentication, doing some light
    css to get a better feel of the page before diving into the
    functionality.

--- assets/scripts/app.js
+++ assets/scripts/app.js
@@ -1,5 +1,6 @@
 'use strict'
 
+const events = require('./events')
 // use require with a reference to bundle the file and use it in this file
 // const example = require('./example')
 
@@ -8,4 +9,16 @@
 
 $(() => {
   // your JS code goes here
+  $('#sign-up-form').hide()
+  $('#sign-in-form').hide()
+  $('#change-password-form').hide()
+  $('#sign-up-button').on('click', () => {
+    $('#sign-up-form').show()
+  })
+  $('#sign-in-button').on('click', () => {
+    $('#sign-in-form').show()
+  })
+  $('#change-password-button').on('click', () => {
+    $('#change-password-form').show()
+  })
 })
