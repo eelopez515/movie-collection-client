2fc648cd5a58b2c3f243a6f004ad63a7cfafc56f
eelopez515 <eelopez15@gmail.com>
2020-09-20 22:02:25 -0400

    update
    was able to display all user movies with only the movie credentials, and
    added a delete and save button to each movie to use for patch and
    delete.

--- assets/scripts/movie/ui.js
+++ assets/scripts/movie/ui.js
@@ -10,10 +10,44 @@ const onAddMovieSuccess = function (response) {
 const onAddMovieFailure = function () {
   $('#message').text('Movie was not add, try again.')
 }
-const onShowMoviesSuccess = function (movies) {
-  console.log(movies)
+const onShowMoviesSuccess = function (response) {
+  console.log(response)
   $('#message').text('Here is your movie collection, ' + store.user.email)
-  $('#collection-view').text(movies)
+  // const moviesString = JSON.stringify(movies)
+  // console.log(movies.movies[0].title)
+  // const title = movies.movies[0].title
+  const collection = response.movies
+  for (let i = 0; i < collection.length; i++) {
+    const heading = document.createElement('h5')
+    const title = document.createElement('p')
+    const director = document.createElement('p')
+    const releaseYear = document.createElement('p')
+    const genre = document.createElement('p')
+    const rating = document.createElement('p')
+    const reviews = document.createElement('p')
+    const update = document.createElement('Button')
+    const remove = document.createElement('Button')
+
+    heading.textContent = 'Movie'
+    title.textContent = 'Title: ' + collection[i].title
+    director.textContent = 'Director: ' + collection[i].director
+    releaseYear.textContent = 'Release Year: ' + collection[i].releaseYear
+    genre.textContent = 'Genre: ' + collection[i].genre
+    rating.textContent = 'Rating: ' + collection[i].rating
+    reviews.textContent = 'Review: ' + collection[i].reviews
+    update.textContent = 'Update'
+    remove.textContent = 'Delete'
+
+    document.getElementById('collection-view').appendChild(heading)
+    document.getElementById('collection-view').appendChild(title)
+    document.getElementById('collection-view').appendChild(director)
+    document.getElementById('collection-view').appendChild(releaseYear)
+    document.getElementById('collection-view').appendChild(genre)
+    document.getElementById('collection-view').appendChild(rating)
+    document.getElementById('collection-view').appendChild(reviews)
+    document.getElementById('collection-view').appendChild(update)
+    document.getElementById('collection-view').appendChild(remove)
+  }
 }
 const onShowMoviesFailure = function (response) {
   console.log(response)
